2000c9c19e6dfcfed61576661c663a3ccd3be1a8
Zeinab Nasiri <96521828+zeinab-nsr@users.noreply.github.com>
2022-08-22 11:40:13 +0430

    CI: merge main to master

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from'./components/Header/index'
+import './App.scss';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+
+function Header() {
+  return (
+    <header className='header'>
+      <span>
+        <b>Room1</b>
+        Now voting: Add new header to home
+      </span>
+      <button class='invite-btn'>
+        Invite Players
+        <i></i>
+      </button>
+    </header>  
+  );
+}
+
+export default Header;
