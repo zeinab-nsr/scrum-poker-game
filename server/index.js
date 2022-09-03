b4c354b5fd3206f637c334a9aa1ea4eecacb3305
Zeinab Nasiri <zeinab.nasiri1996@gmail.com>
2022-09-03 17:43:50 +0430

    FEATURE: add backend basic codes and server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,60 @@
+const express = require("express");
 const http = require("http");
-import { WebSocketServer } from "ws";
-const port = 3001;
+const app = express();
+const socket = require("socket.io");
+const server = http.createServer(app);
+const io = socket(server);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const users = [];
+const messages = {
+  general: [],
+  random: [],
+  jokes: [],
+  javascript: [],
+}
+
+io.on("connection", socket => {
+  socket.on("join server", (username) => {
+    const user = {
+      username,
+      id: socket.id,
+    }
+    users.push(user);
+    io.emit("new user", users);
+  });
+
+  socket.on("join room", (roomName, cb) => {
+    socket.join(roomName);
+    cb(messages[roomName]);
+  });
+
+  socket.on("send message", ({content, to, sender, chatName, isChannel}) => {
+    if (isChannel) {
+      const payload = {
+        content,
+        chatName,
+        sender,
+      }
+      socket.to(to).emit("new message", payload);
+    } else {
+      const payload = {
+        content,
+        chatName: sender,
+        sender,
+      }
+    } 
+    if (messages[chatName]) {
+      messages[chatName].push ({
+        sender,
+        content
+      })
+    }
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.io);
+    io.emit("new user", users);
+  });
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://${port}/`);
-});
+app.listen(3000, () => {console.log('server is runnig on port 3000...')});
