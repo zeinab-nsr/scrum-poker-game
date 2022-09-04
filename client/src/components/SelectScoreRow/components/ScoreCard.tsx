52ac471d22b05d971580bca18e2d60fa6cb2bb99
Zeinab Nasiri <zeinab.nasiri1996@gmail.com>
2022-09-04 11:42:22 +0430

    FIX: add dynamic psuedo element content using styled-components

--- client/src/components/SelectScoreRow/components/ScoreCard.tsx
+++ client/src/components/SelectScoreRow/components/ScoreCard.tsx
@@ -1,51 +1,75 @@
 import React from "react";
+import styled from 'styled-components'
 
 function ScoreCard() {
+
+    const StyledScore = styled.div`
+    &:after {
+      content: "${props => props.defaultValue}";
+    }
+    &:before {
+      content: "${props => props.defaultValue}";
+    }
+  `;
+
     return (
       <section className="score-wrapper">
         <div className="score-row">
           <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="1">
                   1
-              </div>
+              </StyledScore>
             </div>
             <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="2">
                   2
-              </div>
+              </StyledScore>
             </div>
             <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="3">
                   3
-              </div>
+              </StyledScore>
             </div>
             <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="5">
                   5
-              </div>
+              </StyledScore>
             </div>
         </div>
 
         <div className="score-row">
           <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="8">
                   8
-              </div>
+              </StyledScore>
             </div>
             <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="13">
                   13
-              </div>
+              </StyledScore>
             </div>
             <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="20">
                   20
-              </div>
+              </StyledScore>
             </div>
             <div className="score-card">
-              <div className="score">
+              <StyledScore className="score" defaultValue="40">
                   40
-              </div>
+              </StyledScore>
+            </div>
+        </div>
+
+        <div className="score-row">
+          <div className="score-card">
+              <StyledScore className="score" defaultValue="100">
+                  100
+              </StyledScore>
+            </div>
+            <div className="score-card">
+              <StyledScore className="score" defaultValue="?">
+                ?
+              </StyledScore>
             </div>
         </div>
       </section>
