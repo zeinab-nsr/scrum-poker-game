596c7ee022e44f2877ad687c4d10885b7f721bf9
Zeinab Nasiri <zeinab.nasiri1996@gmail.com>
2022-09-04 10:54:01 +0430

    FEATURE: add score cards

--- client/src/components/SelectScoreRow/components/ScoreCard.tsx
+++ client/src/components/SelectScoreRow/components/ScoreCard.tsx
@@ -2,11 +2,54 @@ import React from "react";
 
 function ScoreCard() {
     return (
-      <div className="score-card">
-        <div className="score">
-            2
+      <section className="score-wrapper">
+        <div className="score-row">
+          <div className="score-card">
+              <div className="score">
+                  1
+              </div>
+            </div>
+            <div className="score-card">
+              <div className="score">
+                  2
+              </div>
+            </div>
+            <div className="score-card">
+              <div className="score">
+                  3
+              </div>
+            </div>
+            <div className="score-card">
+              <div className="score">
+                  5
+              </div>
+            </div>
         </div>
-      </div>
+
+        <div className="score-row">
+          <div className="score-card">
+              <div className="score">
+                  8
+              </div>
+            </div>
+            <div className="score-card">
+              <div className="score">
+                  13
+              </div>
+            </div>
+            <div className="score-card">
+              <div className="score">
+                  20
+              </div>
+            </div>
+            <div className="score-card">
+              <div className="score">
+                  40
+              </div>
+            </div>
+        </div>
+      </section>
+      
     );
   }
   
