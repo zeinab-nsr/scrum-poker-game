048679761d0c5a82cb2e6facb5cc46a38e4f56df
Maryam Rahimi <mrahimi.jp@gmail.com>
2022-10-18 16:16:09 +0330

    backend base config created

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,64 +1,22 @@
 const express = require("express");
-const http = require("http");
 const app = express();
-const socket = require("socket.io");
-const server = http.createServer(app);
-const io = socket(server);
+const http = require("http");
+const bodyParser = require('body-parser');
 
-const users = [];
-const messages = {
-  general: [],
-  random: [],
-  jokes: [],
-  javascript: [],
+function App() {
+  const server = http.createServer(app);
+
+  // configs
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // APIs
+
+
+  server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  })
+
 }
 
-app.get('/express_backend', (req, res) => {
-  res.send('Hello World!')
-})
-
-io.on("connection", socket => {
-  socket.on("join server", (username) => {
-    const user = {
-      username,
-      id: socket.id,
-    }
-    users.push(user);
-    io.emit("new user", users);
-  });
-
-  socket.on("join room", (roomName, cb) => {
-    socket.join(roomName);
-    cb(messages[roomName]);
-  });
-
-  socket.on("send message", ({content, to, sender, chatName, isChannel}) => {
-    if (isChannel) {
-      const payload = {
-        content,
-        chatName,
-        sender,
-      }
-      socket.to(to).emit("new message", payload);
-    } else {
-      const payload = {
-        content,
-        chatName: sender,
-        sender,
-      }
-    } 
-    if (messages[chatName]) {
-      messages[chatName].push ({
-        sender,
-        content
-      })
-    }
-  });
-
-  socket.on("disconnect", () => {
-    users = users.filter(u => u.id !== socket.io);
-    io.emit("new user", users);
-  });
-});
-
-server.listen(3000);
+module.exports = App;
