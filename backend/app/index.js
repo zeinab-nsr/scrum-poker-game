d381832c089780e160359f2b6a91f31c395f7539
Maryam Rahimi <mrahimi.jp@gmail.com>
2022-10-18 17:17:39 +0330

    fix:server port chaged

--- backend/app/index.js
+++ backend/app/index.js
@@ -12,9 +12,8 @@ function App() {
 
   // APIs
 
-
-  server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  server.listen(3002, () => {
+    console.log("Server is running on port 3002");
   })
 
 }
