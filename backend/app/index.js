ba5795526810d2d9a896f8ea4ce81efd70b09475
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2022-11-01 13:22:15 +0330

    CHORE: add socket io config

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,21 +1,69 @@
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = express();
-const http = require("http");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 const bodyParser = require('body-parser');
 
 function App() {
-  const server = http.createServer(app);
-
   // configs
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  // APIs
+  // socket events
+  io.on("connection", (socket) => { // socket instance
+    socket.emit("hello", "world");
+  });
 
-  server.listen(3002, () => {
+  // APIs
+  httpServer.listen(3002, () => {
     console.log("Server is running on port 3002");
   })
-
 }
 
 module.exports = App;
+
+// io.on("connection", socket => {
+//   socket.on("join server", (username) => {
+//     const user = {
+//       username,
+//       id: socket.id,
+//     }
+//     users.push(user);
+//     io.emit("new user", users);
+//   });
+
+//   socket.on("join room", (roomName, cb) => {
+//     socket.join(roomName);
+//     cb(messages[roomName]);
+//   });
+
+//   socket.on("send message", ({content, to, sender, chatName, isChannel}) => {
+//     if (isChannel) {
+//       const payload = {
+//         content,
+//         chatName,
+//         sender,
+//       }
+//       socket.to(to).emit("new message", payload);
+//     } else {
+//       const payload = {
+//         content,
+//         chatName: sender,
+//         sender,
+//       }
+//     } 
+//     if (messages[chatName]) {
+//       messages[chatName].push ({
+//         sender,
+//         content
+//       })
+//     }
+//   });
+
+//   socket.on("disconnect", () => {
+//     users = users.filter(u => u.id !== socket.io);
+//     io.emit("new user", users);
+//   });
+// });
