2991bbc7428e31659f5d9b3b2b3cf9d5c8294c4c
Zeinab Nasiri <zeinab.nasiri1996@gmail.com>
2022-11-07 11:07:17 +0330

    FEATURE: add blink animation to dots

--- frontend/src/components/Result/index.tsx
+++ frontend/src/components/Result/index.tsx
@@ -3,7 +3,10 @@ import React from "react";
  function Result() {
     return (
       <section className="result-desk">
-        Waiting for player's votes...
+        Waiting for player's votes
+        <span className="blinking-dot">.</span>
+        <span className="blinking-dot">.</span>
+        <span className="blinking-dot">.</span>
       </section>  
     );
   } 
