58ec33d30e15522a8181cc796e751b2e3c2a25c0
Zeinab Nasiri <zeinab.nasiri1996@gmail.com>
2022-11-13 10:15:25 +0330

    FEATURE: select score card onClick

--- frontend/src/components/Score/components/ScoreCard.tsx
+++ frontend/src/components/Score/components/ScoreCard.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components'
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 
-const socket = io()
+const socket = io();
 
 function ScoreCard() {
-
-  const scores = [1, 2, 3, 5, 8, 13, 20, 40, 100, '?']
+  const [selectedScore, setSelectedScore] = useState<string | number>('');
+  const scores = [1, 2, 3, 5, 8, 13, 20, 40, 100, '?'];
 
   const StyledScore = styled.div`
     &:after {
@@ -17,17 +17,17 @@ function ScoreCard() {
     }
   `;
 
-  function addScore(score: string | number) {
+  function handleSelectScore(score: string | number) {
+    setSelectedScore(score);
     const userId = sessionStorage.getItem("id");
     socket.emit("add score", {score, userId});
-
   }
 
   return (
     <section className="score-wrapper">
       <div className="score-row">
         {scores.map(score =>
-          <div key={score} className="score-card" onClick={() => addScore(score)}>
+          <div key={score} className={selectedScore !== score ? "score-card" : "score-card selected"} onClick={() => handleSelectScore(score)}>
             <StyledScore className="score" defaultValue={score}>
               {score}
             </StyledScore>
