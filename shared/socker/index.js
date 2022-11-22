bee993c03f551bded94bc0e5f0ab13f855e17fb1
Maryam Rahimi <mrahimi.jp@gmail.com>
2022-11-22 11:55:52 +0330

    REFACTOR: structure changed

--- shared/socker/index.js
+++ shared/socker/index.js
@@ -1,12 +1,5 @@
-const events = {
-  DISCONNECT: 'DISCONNECT',
-  JOIN_ROOM: 'JOIN_ROOM',
-  USERS_MODIFIED: 'USERS_MODIFIED',
-  GET_CONNECTION_ID: 'GET_CONNECTION_ID',
-  ADD_SCORE: 'ADD_SCORE',
-  USER_VOTED: 'USER_VOTED',
-  SET_AVG: 'SET_AVG',
-  GET_AVG: 'GET_AVG'
-};
+const Events = require('./Events').default;
 
-module.exports = { events }
+module.exports = {
+  Events
+};
